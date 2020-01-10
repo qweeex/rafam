506fbe6ebd3e00f9529dab1aeca357717b187803
Arkady Pugachev <qweeex@yandex.com>
2020-01-10 22:31:34 +0400

    Mobile version 5%

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
 $(document).ready(() => {
 
+
+    // Выбор города
+    $('.header-city__btn').on('click', (e) => {
+        e.preventDefault();
+        $('.header-city').toggleClass('header-city_active');
+    });
+
+    // Меню 
+    $('.navbar-mobile__btn').on('click', (e) => {
+        e.preventDefault();
+        $('.navbar-mobile__btn').toggleClass('navbar-mobile__btn-active');
+        $('.navbar-mob').toggleClass('navbar-mob_active');
+    });
+
     // Слайдер
     var $input = $(".aroma-size__num");
     $(".aroma-range").ionRangeSlider({
@@ -16,5 +30,7 @@ $(document).ready(() => {
             $input.prop("value", data.from);
         }
     });
+
+    
 
 });
